8ef47744308bf39dad9245e91387e6b39a1fd041
extinctionsq <jasonmiracle56@gmail.com>
2019-07-30 21:04:39 +0800

    v3

--- app/src/main/java/com/gevyo/android/hfosapp/HomePage.java
+++ app/src/main/java/com/gevyo/android/hfosapp/HomePage.java
@@ -9,18 +9,29 @@ import android.widget.Toast;
 
 import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomePage extends AppCompatActivity {
     Button btnView,btnInsert,btnLogout;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        database = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
         btnView = findViewById(R.id.view);
         btnInsert = findViewById(R.id.insert);
         btnLogout = findViewById(R.id.logout);
+
+        final DatabaseReference myref = database.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        myref.child("user").child(user.getUid()).child("email").setValue(user.getEmail());
     }
     public void onClick(View view){
         if(view == btnView){
