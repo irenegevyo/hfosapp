bc9f3322e91e52e002f712d6eeeadce5558c71b8
irenegevyo <irenegevyo@gmail.com>
2019-07-30 16:26:41 +0700

    insertview

---
+++ app/src/main/java/com/gevyo/android/hfosapp/InsertPage.java
@@ -0,0 +1,13 @@
+package com.gevyo.android.hfosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_page);
+    }
+}
